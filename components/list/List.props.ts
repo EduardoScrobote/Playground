9d621c0cbbc7c166653884b1faded482c1f86e8a
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-01-30 09:06:05 -0300

    add: add new project

--- components/list/List.props.ts
+++ components/list/List.props.ts
@@ -3,4 +3,8 @@ export const Projects = [
     name: "Precisão Adaptativa",
     path: "https://adaptative-precision.vercel.app/",
   },
+  {
+    name: "Modelo Login",
+    path: "https://auth-template-seven.vercel.app/",
+  },
 ];
