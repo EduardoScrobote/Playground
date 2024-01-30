76f26540274b64bb5f8185447e4dffc089b1b5be
Eduardo Scrobote <EduardoScroboteProfissional@gmail.com>
2024-01-30 09:14:10 -0300

    fix: playground-routes

--- components/list/List.tsx
+++ components/list/List.tsx
@@ -9,9 +9,7 @@ function List() {
       {Projects.map(({ name, path }, i) => (
         <div key={i} className="flex flex-col w-full">
           <div
-            onClick={() =>
-              window.open("https://adaptative-precision.vercel.app/")
-            }
+            onClick={() => window.open(`${path}`)}
             className="flex flex-row border-b-2 hover:text-slate-500 hover:cursor-pointer border-purple-600 mb-10 justify-between w-[100%]"
           >
             <h1 className="text-2xl font-semibold">{`00${i + 1}`}</h1>
